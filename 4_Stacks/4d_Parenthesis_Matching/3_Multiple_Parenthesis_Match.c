1e01c3d5723f12875283c0c9a79961c97352546e
Kartik <itzkartikeds3@gmail.com>
2023-10-04 05:25:53 +0530

    Parenthesis Problems all cases done

--- 4_Stacks/4d_Parenthesis_Matching/3_Multiple_Parenthesis_Match.c
+++ 4_Stacks/4d_Parenthesis_Matching/3_Multiple_Parenthesis_Match.c
@@ -60,26 +60,52 @@ char pop(struct stack *ptr)
     }
 }
 
-int parenthesisMatch(char *exp){
+int match(char a, char b){
+    if(a == '{' && b == '}'){
+        return 1;
+    }
+    if(a == '[' && b == ']'){
+        return 1;
+    }
+    if(a == '(' && b == ')'){
+        return 1;
+    }
+    return 0;
+}
+
+int parenthesisMatch(char *exp)
+{
     // Create and Initialize the stack
     struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
-    sp->size=100;
+    sp->size = 100;
     sp->top = -1;
     sp->arr = (char *)malloc(sp->size * sizeof(char));
+    char popped_ch;
 
-    for(int i=0; exp[i]!='\0'; i++){
-        if(exp[i] == '('){
-            push(sp, '(');
-        } else if(exp[i] == ')'){
-            if(isEmpty(sp)){
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+        {
+            push(sp, exp[i]);
+        }
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (isEmpty(sp))
+            {
                 return 0;
             }
-            pop(sp);
+            popped_ch = pop(sp);
+            if(!match(popped_ch, exp[i])){
+                return 0;
+            }
         }
     }
-    if(isEmpty(sp)){
+    if (isEmpty(sp))
+    {
         return 1;
-    } else{
+    }
+    else
+    {
         return 0;
     }
 }
@@ -87,15 +113,15 @@ int parenthesisMatch(char *exp){
 int main()
 {
 
-    char *exp = "((8)*(9))";
+    char *exp = "[4-6]((8)*{(9-8)})";
 
     if (parenthesisMatch(exp))
     {
-        printf("The parenthesis is matching\n");
+        printf("The parenthesis is balanced\n");
     }
     else
     {
-        printf("The parenthesis is not matching\n");
+        printf("The parenthesis is not balanced\n");
     }
 
     return 0;
